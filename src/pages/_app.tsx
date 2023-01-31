79b47e124eaba87ce33de7d5089cda326636cbb3
jinn0106 <jin0106@kslab.co.kr>
2023-01-31 10:43:42 +0900

    feat : msw 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
+if (process.env.NODE_ENV === "development") {
+    if (typeof window !== undefined) {
+        async () => {
+            const { worker } = await import("../mocks/browser");
+            worker.start();
+        };
+    } else {
+        async () => {
+            const { server } = await import("../mocks/server");
+            server.listen();
+        };
+    }
+}
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
