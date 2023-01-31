4eb6657d97066defe217e43f49023682b1d3af96
jinn0106 <jin0106@kslab.co.kr>
2023-01-31 10:23:54 +0900

    feat : handler 작성

---
+++ src/mocks/handler.ts
@@ -0,0 +1,26 @@
+import { rest } from "msw";
+
+export const handlers = [
+    rest.post("/login", (req, res, ctx) => {
+        sessionStorage.setItem("is-authenticated", "true");
+        return res(ctx.status(200));
+    }),
+    rest.get("/user", (req, res, ctx) => {
+        const isAuthenticated = sessionStorage.getItme("is-authenticated");
+
+        if (!isAuthenticated) {
+            return res(
+                ctx.status(403),
+                ctx.json({
+                    errorMessage: "Not authorized",
+                }),
+            );
+        }
+        return res(
+            ctx.status(200),
+            ctx.json({
+                userName: "admin",
+            }),
+        );
+    }),
+];
